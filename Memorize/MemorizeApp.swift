2ec6133d9c7e0267f9ff85097f2f6e93ce9ca8de
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2021-11-04 15:00:26 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Joseph A Guzzardo on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
